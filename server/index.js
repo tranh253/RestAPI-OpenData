d04c415454a2a45802febb180c19c4c47cb747ee
tranh253 <trunganh25397@gmail.com>
2021-09-27 21:04:20 +0300

    Add heroku.yml

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const axios = require('axios');
 const token = process.env.REACT_APP_API_KEY;
+const express = require('express');
+const app = express();
+        
+app.listen('3001', () => { })
 axios.get("https://opendata.hopefully.works/api/events", {
           headers: { Authorization: `Bearer ${token}` },
         }).then(
@@ -10,3 +14,5 @@ axios.get("https://opendata.hopefully.works/api/events", {
         ).catch(error => {
             console.log(error);
         });
+
+       
